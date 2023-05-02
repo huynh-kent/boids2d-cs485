a5257a5ea2e391a9ec468d7686e8e75931d3b339
huynh-kent <huynhkentd@gmail.com>
2023-05-01 23:42:02 -0700

    added position vector

--- target/target_sketch.js
+++ target/target_sketch.js
@@ -1647,24 +1647,35 @@ windowResized = None
 `;
 
 let userCode = `
-import random
+#import random
+#import numpy as np
+#from js import p5
+
 class Boid:
     def __init__ (self):
-        self.x = random.randrange(0+radius,600-radius)
-        self.y = random.randrange(0+radius,400-radius)
+        #self.x = random.randrange(0+radius,600-radius)
+        #self.y = random.randrange(0+radius,400-radius)
+        self.position = createVector([100],[100])
+        #self.position = p5.Vector([100], [1])
+        self.velocity = p5.Vector.random2D()
+        self.velocity.setMag(1,1)
+        #self.acceleration = createVector(1,1)
+        
         
     def show(self):
         fill("blue")
         diameter = sin(frameCount / 60) * 50 + 50
-        ellipse(self.x, self.y, diameter, diameter)
+        ellipse(self.position.x, self.position.y, diameter, diameter)
+        #print(self.position)
+        #print(f'position {self.position.x}')
         
 diameter=100
 #diameter = sin(frameCount / 60) * 50 + 50
 radius=diameter/2
-flock = [Boid() for i in range(10)]
 
 def setup():
     createCanvas(600, 400)
+    flock = [Boid() for i in range(10)]
 
 def draw():
     background(200)
